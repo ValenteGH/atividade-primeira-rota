f866eb7082dcfb0cf1cfa442af7b511e83bb2031
Valente <101368268+ValenteGH@users.noreply.github.com>
2022-03-17 22:47:38 +0000

    rota

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ app = Flask('app')
 
 @app.route('/')
 def hello_world():
-  return '<h1>Minha primeira rota!</h1>'
-
-app.run(host='0.0.0.0', port=8080)
+  return '<h1>Olá Mundo!</h1>'
+  
+@app.route('/unifran')
+def unifran():
+  return'<h2>Universidade de Franca</h2>'
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
